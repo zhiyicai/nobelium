0f84883d4c2bdcece0cd7d58a9d51d78dbad0983
zhiyicai <2857483@qq.com>
2022-05-02 23:22:44 +0200

    删除了 Vercel

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
   title: 'CRAIGARY',
-  author: 'Craig Hart',
+  author: 'Zhiyi Cai',
   email: 'i@craigary.net',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
